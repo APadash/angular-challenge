75e7d9ae7f9aaa3e261e9ccf45f5e64532da7fa8
Abbas Padash <ap.prog1@gmail.com>
2023-07-16 20:17:04 +0330

    lazy loading was implemented.

--- ticket-challenge/src/app/app-routing.module.ts
+++ ticket-challenge/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'salons',
+    loadChildren: () => import('./salons-list/salons-list.module').then((m) => m.SalonsListModule)
+  },
+  {
+    path: 'plan',
+    loadChildren: () => import('./plan/plan.module').then((m) => m.PlanModule)
+  },
+  { path: '', redirectTo: 'salons', pathMatch: 'full'},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ticket-challenge/src/app/app.module.ts
+++ ticket-challenge/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlanComponent } from './plan/plan.component';
-import { SalonsListComponent } from './salons-list/salons-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PlanComponent,
-    SalonsListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
